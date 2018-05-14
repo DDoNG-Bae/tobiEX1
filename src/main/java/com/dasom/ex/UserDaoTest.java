6ee051ff52254d39cb5941619139989133d11273
Dasom Lim <ektha03@gmail.com>
2018-05-15 05:07:19 +0900

    rename Main.java to UserDaoTest

--- src/main/java/com/dasom/ex/UserDaoTest.java
+++ src/main/java/com/dasom/ex/UserDaoTest.java
@@ -2,7 +2,7 @@ package com.dasom.ex;
 
 import java.sql.SQLException;
 
-public class Main {
+public class UserDaoTest {
 
 	public static void main(String[] args) throws ClassNotFoundException,SQLException {
 		// TODO Auto-generated method stub
