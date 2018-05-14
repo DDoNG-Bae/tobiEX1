b07df617c6b1ba6bddbebab372ba268ffb8a1a3a
Dasom Lim <ektha03@gmail.com>
2018-05-15 05:13:22 +0900

    1.5 스프링의 IoC

--- src/main/java/com/dasom/ex/UserDaoTest.java
+++ src/main/java/com/dasom/ex/UserDaoTest.java
@@ -2,12 +2,17 @@ package com.dasom.ex;
 
 import java.sql.SQLException;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class UserDaoTest {
 
 	public static void main(String[] args) throws ClassNotFoundException,SQLException {
 		// TODO Auto-generated method stub
 		
-		UserDao dao = new UserDao(new SimpleConnectionMaker());
+		ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+		
+		UserDao dao = context.getBean("userDao", UserDao.class);
 		
 		User user = new User();
 		user.setId("ektha1");
