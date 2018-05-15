3a6c2da57383046c7460209e17f48d6c5af30003
Dasom Lim <ektha03@gmail.com>
2018-05-16 05:27:50 +0900

    1.8.1~1.8.2 XML설정, XML을 이용하는 애플리케이션 컨텍스트

--- src/main/java/com/dasom/ex/UserDaoTest.java
+++ src/main/java/com/dasom/ex/UserDaoTest.java
@@ -4,18 +4,20 @@ import java.sql.SQLException;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 
 public class UserDaoTest {
 
 	public static void main(String[] args) throws ClassNotFoundException,SQLException {
 		// TODO Auto-generated method stub
 		
-		ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+		ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
 		
 		UserDao dao = context.getBean("userDao", UserDao.class);
 		
 		User user = new User();
-		user.setId("ektha0");
+		user.setId("ektha4");
 		user.setName("다솜");
 		user.setPassword("ektha");
 		
